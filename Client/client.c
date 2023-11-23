05d15c630fb2f5363e17ca269a149e6c2686877e
Takehit0Matsuda <mattsutaking@gmail.com>
2023-11-23 08:58:00 +0000

    modify comments

--- Client/client.c
+++ Client/client.c
@@ -11,13 +11,13 @@
 #define END_FLAG '\n'
 
 int main(int argc, char const* argv[]) {
-    
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    
+
+
     int status, valread, client_fd;    // status: return value of connect(), valread: return value of read(), client_fd: client socket descriptor
     struct sockaddr_in serv_addr;      // server address structure
     char* hello = "Hello from client"; // message to be sent
@@ -25,7 +25,7 @@ int main(int argc, char const* argv[]) {
 
     // Socket creation
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n"); // error handling if socket creation fails
+        printf("\n Socket creation error \n"); // Error handling if socket creation fails
         return -1;
     }
 
@@ -34,57 +34,57 @@ int main(int argc, char const* argv[]) {
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n"); // error handling if address conversion fails
+        printf("\nInvalid address/ Address not supported \n"); // Error handling if address conversion fails
         return -1;
     }
 
     // Establishing a connection to the server
     if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("\nConnection Failed \n"); // error handling if connection fails
+        printf("\nConnection Failed \n"); // Error handling if connection fails
         return -1;
     }
-    
+
     // Requesting the file name from the user
     printf("Enter file to retrieve: ");
     fgets(buffer, BUFFER_SIZE, stdin);
-    buffer[strlen(buffer) - 1] = END_FLAG;  // メッセージの終わりを示すフラグを追加
+    buffer[strlen(buffer) - 1] = END_FLAG;  // Add the end-of-message flag
     // Remove the trailing newline character
     size_t len = strlen(buffer);
     if (len > 0 && buffer[len - 1] == '\n') {
         buffer[len - 1] = '\0';
     }
-    
+
     printf("Connecting on Localhost at port %d to get file %s.\n", atoi(argv[1]), buffer);
-    
+
     // Sending the file name to the server
     send(client_fd, buffer, strlen(buffer), 0);
-    
-    // クライアントからファイルサイズを受信
+
+    // Receive the file size from the client
     size_t file_size;
     ssize_t valread_size = read(client_fd, &file_size, sizeof(file_size));
     if (valread_size != sizeof(file_size)) {
         perror("Error reading file size");
-        // エラーハンドリング
+        // Error handling
         return -1;
     }
     printf("File Size: %zu\n", file_size);
-    
-    // ファイルサイズ分だけデータを受信
+
+    // Receive data for the file size
     size_t total_received = 0;
     while (total_received < file_size) {
         ssize_t bytes_received = read(client_fd, buffer, BUFFER_SIZE - 1);
         if (bytes_received <= 0) {
             break;
         }
-        buffer[bytes_received] = '\0';  // ヌル文字を追加してC文字列を完成させる
-        printf("%.*s", (int)bytes_received, buffer);  // 受信したデータを表示
+        buffer[bytes_received] = '\0';  // Add null character to complete the C string
+        printf("%.*s", (int)bytes_received, buffer);  // Display the received data
         total_received += bytes_received;
     }
-    
-    // ENDの後に改行を追加
+
+    // Add a newline after END
     printf("\n");
-    
-    // Closing the connected socket
+
+    // Close the connected socket
     close(client_fd);
 
     return 0;
--- Server/server.c
+++ Server/server.c
@@ -5,14 +5,14 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>  // 追加：スレッド関連のヘッダーファイル
+#include <pthread.h>  // Added: Header file for threads
 #include <limits.h>
 
 //#define PORT 8080
 #define BUFFER_SIZE 1024
-#define END_FLAG '\n'  // 追加：メッセージの終わりを示すフラグ
+#define END_FLAG '\n'  // Added: Flag indicating the end of a message
 
-// 追加：スレッド用の関数
+// Added: Function for threads
 void *handle_request(void *socket_desc) {
     int new_socket = *(int *)socket_desc;
     ssize_t valread;
@@ -20,60 +20,60 @@ void *handle_request(void *socket_desc) {
 
     // Receiving the file name from the client
     valread = read(new_socket, buffer, BUFFER_SIZE - 1);
-    buffer[valread] = '\0';  // ヌル文字を追加してC文字列を完成させる
+    buffer[valread] = '\0';  // Add null character to complete the C string
     printf("From Client (%zd): %s\n", valread, buffer);
-    // ファイルのパスを構築
-    char file_path[PATH_MAX];  // PATH_MAX は標準で定義されている最大パス長
+    // Build the file path
+    char file_path[PATH_MAX];  // PATH_MAX is the standard definition for the maximum path length
     snprintf(file_path, sizeof(file_path), "root/%s", buffer);
-
 
     // TODO: Open the file, get its size, and send the size to the client
     printf("Opening root/%s\n", buffer);
-    FILE *file = fopen(file_path, "rb");  // ファイルをバイナリモードで開く
-    
+    FILE *file = fopen(file_path, "rb");  // Open the file in binary mode
+
     if (file == NULL) {
         perror("Error opening file");
-        // クライアントにエラーメッセージを送信するなどの処理を追加することが考えられます
+        // Additional processing, such as sending an error message to the client, could be added here
     } else {
-        fseek(file, 0, SEEK_END);  // ファイルの終端まで移動
-        size_t file_size = ftell(file);  // ファイルサイズを取得
+        fseek(file, 0, SEEK_END);  // Move to the end of the file
+        size_t file_size = ftell(file);  // Get the file size
         printf("File is %zd bytes\n", file_size);
-        fseek(file, 0, SEEK_SET);  // ファイルの先頭に戻る
-    
-        // クライアントにファイルサイズを送信
-        printf("Sending %zd\n", file_size);    
+        fseek(file, 0, SEEK_SET);  // Move to the beginning of the file
+
+        // Send the file size to the client
+        printf("Sending %zd\n", file_size);
         send(new_socket, &file_size, sizeof(file_size), 0);
-    
+
+        // Display the file contents
         size_t total_sent = 0;
         size_t bytes_read;
-    
+
         while (total_sent < file_size) {
             bytes_read = fread(buffer, 1, sizeof(buffer), file);
-            printf("%.*s", (int)bytes_read, buffer);
+            printf("%.*s", (int)bytes_read, buffer);  // Display the received data
             send(new_socket, buffer, bytes_read, 0);
             total_sent += bytes_read;
         }
-    
-        fclose(file);  // ファイルを閉じる
+
+        fclose(file);  // Close the file
     }
 
-    // クライアントに処理が終了したことを伝える
+    // Notify the client that the processing has finished
     send(new_socket, "END\n", strlen("END\n"), 0);
 
-    // Closing the connected socket
+    // Close the connected socket
     close(new_socket);
 
-    // スレッドを終了
+    // Exit the thread
     pthread_exit(NULL);
 }
 
 int main(int argc, char const* argv[]) {
-    
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
+
     int server_fd, new_socket;      // server_fd: server socket descriptor, new_socket: socket descriptor for communication with the client
     ssize_t valread;                // data type for the return value of read() function (number of bytes read)
     struct sockaddr_in address;     // socket address structure
@@ -84,13 +84,13 @@ int main(int argc, char const* argv[]) {
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");    // error handling if socket creation fails
+        perror("socket failed");    // Error handling if socket creation fails
         exit(EXIT_FAILURE);
     }
 
     // Forcefully attaching socket to the port
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");       // error handling if setting socket options fails
+        perror("setsockopt");       // Error handling if setting socket options fails
         exit(EXIT_FAILURE);
     }
 
@@ -98,21 +98,21 @@ int main(int argc, char const* argv[]) {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(atoi(argv[1]));
-    
+
     printf("Launching Server on Port: %d\n", atoi(argv[1]));
 
     // Forcefully attaching socket to the port
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");       // error handling if binding socket and address fails
+        perror("bind failed");       // Error handling if binding socket and address fails
         exit(EXIT_FAILURE);
     }
 
     // Listening for client connections
     if (listen(server_fd, 3) < 0) {
-        perror("listen");           // error handling if setting up listening fails
+        perror("listen");           // Error handling if setting up listening fails
         exit(EXIT_FAILURE);
     }
-    
+
     // Accepting connections and launching threads for each request
     while (1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
